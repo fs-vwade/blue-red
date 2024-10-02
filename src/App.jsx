b832e922ea59740df6984ba500bc3e4a98b76c49
Victor Wade <p2525657888@gmail.com>
2024-10-02 11:29:42 -1000

    import feature components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import { Route, Routes } from "react-router-dom";
 import { useState } from "react";
+import Red from "./features/pages/Red";
+import Blue from "./features/pages/Blue";
+import Home from "./features/pages/Home";
 
 function App() {
 	const [count, setCount] = useState(0);
